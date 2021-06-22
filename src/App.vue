6bcc1a97d5be693485d4033fe6d25bd7419633c8
Song Huang <songhuangcn@qq.com>
2021-06-22 23:00:31 +0800

    finish home page

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,59 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar app color="primary" dark clipped-right>
+      最新文章
     </v-app-bar>
-
+    <v-navigation-drawer app bottom clipped right>
+      <v-container fluid>
+        <v-img
+          :src="require('./assets/hdgcs-barcode.jpg')"
+          contain
+        />
+      </v-container>
+    </v-navigation-drawer>
     <v-main>
-      <HelloWorld/>
+      <v-container fluid>
+        <v-list two-line>
+          <v-list-item v-for="post in posts" :key="post.link" :href="post.link">
+            <v-list-item-avatar>
+              <v-icon class="grey lighten-1" dark>
+                mdi-star-minus
+              </v-icon>
+            </v-list-item-avatar>
+            <v-list-item-content>
+              <v-list-item-title v-text="post.title"></v-list-item-title>
+              <v-list-item-subtitle>{{ post.pubDate | localeDate }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Parser from 'rss-parser'
+import config from './config'
 
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
+  data: function() {
+    return {
+      posts: [],
+    }
   },
-
-  data: () => ({
-    //
-  }),
+  mounted: function() {
+    const parser = new Parser()
+    parser
+      .parseURL(config.wechatRssUrl)
+      .then(feed => {
+        this.posts = feed.items
+      })
+  },
+  filters: {
+    localeDate: function(value) {
+      return new Date(value).toLocaleDateString()
+    },
+  },
 };
 </script>
---
+++ src/config.js
@@ -0,0 +1,3 @@
+export default {
+  wechatRssUrl: 'https://raw.githubusercontent.com/hellodword/wechat-feeds/feeds/MzI5MTAyODM5NQ%3D%3D.xml',
+}
